4133c3d624210e0858a7b38aaea6e85a5a3a153c
andreablushi <andreablushi@gmail.com>
2024-04-28 15:10:28 +0200

    commit problem

--- src/main/java/org/example/configuratoreauto/Utenti/Impiegato.java
+++
@@ -1,9 +0,0 @@
-package org.example.configuratoreauto.Utenti;
-
-import java.io.Serializable;
-
-public class Impiegato extends Persona implements Serializable {
-    public Impiegato(String email, String password){
-        super(email, password);
-    }
-}
--- src/main/java/org/example/configuratoreauto/Utenti/Segretario.java
+++
@@ -1,9 +0,0 @@
-package org.example.configuratoreauto.Utenti;
-
-import java.io.Serializable;
-
-public class Segretario extends Persona implements Serializable {
-    public Segretario(String email, String password){
-        super(email, password);
-    }
-}
